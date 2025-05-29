a0198e1765b7ed131a4a2fca13505bc99257ef2a
McBigMac <jy1996@hotmail.com>
2025-05-29 10:21:03 +0800

    style: add black background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,10 @@ import { Experience } from './components/Experience'
 function App() {
 	return (
 		<div style={{ height: '100vh', width: '100vw' }}>
-			<Canvas camera={{ position: [0, 2.5, 5], fov: 50 }}>
+			<Canvas
+				camera={{ position: [0, 2.5, 5], fov: 50 }}
+				style={{ background: '#000' }}
+			>
 				<Experience />
 			</Canvas>
 		</div>
